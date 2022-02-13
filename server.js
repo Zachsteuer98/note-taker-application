7c6bdae77a8c7e4fe674935663a26c10845a7e06
Zachsteuer98 <Zach.Steuer98@gmail.com>
2022-02-13 12:41:06 -0500

    returned notes.html file and index.html using get routes

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
-const { notes } = require('./data/db.json');
+// const notes = require('./data/db.json');
+// const route = require('./routes/htmlRouters')
 
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-app.get('/api/notes', (req, res) => {
-    res.json(notes);
-  });
+const htmlRoutes = require('./routes/htmlRouters')
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+app.use('/', htmlRoutes);
+
+app.use(express.static('public'));
+
+// app.get('/api/notes', (req, res) => {
+//     res.json(notes);
+//   });
 
   app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
